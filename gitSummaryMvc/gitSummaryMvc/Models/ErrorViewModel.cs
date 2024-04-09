037339c82aa881ed09c0151ff140b2fecf2f3837
GerniVisser-bbd <159510316+GerniVisser-bbd@users.noreply.github.com>
2024-04-09 16:56:23 +0200

    Complete Setup (#4)
    * init
    
    * init: stable version without entityFramework

---
+++ gitSummaryMvc/gitSummaryMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gitSummaryMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
